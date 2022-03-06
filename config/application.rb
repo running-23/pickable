2c7de32ce7c676c91fe9d02e0d4b07658c9b21e0
tarakish <akr.3838soccer@gmail.com>
2022-03-07 04:06:12 +0900

    Perf: rails gコマンド実行時の生成ファイルを制御 #10

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,20 @@ module Pickable
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: false,
+                        request_specs: true,
+                        model_spec: true,
+                        fixtures: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
