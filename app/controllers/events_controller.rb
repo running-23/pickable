27b974afe0fcc63e0d981a2a5226e1d5ca4d60e9
jibiking <daiki.jibiki@gmail.com>
2022-03-08 13:07:03 +0900

    perf: rubocopによる修正 #4

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[show edit update destroy]
 
   def index
     @events = Event.all
@@ -11,14 +11,13 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-
   def edit; end
 
   def create
     @event = Event.new(event_params)
 
     if @event.save
-      redirect_to event_path(@event), notice: "Event was successfully created."
+      redirect_to event_path(@event), notice: 'Event was successfully created.'
     else
       render :new
     end
@@ -26,7 +25,7 @@ class EventsController < ApplicationController
 
   def update
     if @event.update(event_params)
-      redirect_to event_path(@event), notice: "Event was successfully updated."
+      redirect_to event_path(@event), notice: 'Event was successfully updated.'
     else
       render :edit
     end
@@ -35,16 +34,17 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
 
-    redirect_to events_path, notice: "Event was successfully destroyed."
+    redirect_to events_path, notice: 'Event was successfully destroyed.'
   end
 
   private
 
   def set_event
-      @event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   def event_params
-    params.require(:event).permit(:title, :description, :number_of_members, :number_of_applicants, :scheduled_date, :place, :pickable_mode)
+    params.require(:event).permit(:title, :description, :number_of_members, :number_of_applicants,
+                                  :scheduled_date, :place, :pickable_mode)
   end
 end
