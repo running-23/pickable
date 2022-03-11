801fe9890030f277eb76deaa9f87f5f7315850c2
Mokosu-bit <mokosu755@gmail.com>
2022-03-12 03:51:07 +0900

    chore: イベント終了同時に関連した参加を削除するよう追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :event
-  has_many :participations
+  has_many :participations, dependent: :destroy
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
