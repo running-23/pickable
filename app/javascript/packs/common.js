7e83ee998dac64738ff7fa49ce55f401e7b1f4fe
jibiking <daiki.jibiki@gmail.com>
2022-03-17 17:25:09 +0900

    feat: Pickableモードがランダムの時applicantセレクトボックスの非表示 #24

---
+++ app/javascript/packs/common.js
@@ -0,0 +1,15 @@
+  document.addEventListener('DOMContentLoaded', function(){
+    var elem = document.getElementById('selbox');
+    elem.addEventListener('change', function(){
+      var result = elem.value;
+
+      if (result == "random") {
+        //非表示
+          document.getElementById("applicants").style.display = "none";
+      } else {
+        //表示
+        document.getElementById("applicants").style.display = "block";
+      }
+
+    }, false);
+  }, false);
