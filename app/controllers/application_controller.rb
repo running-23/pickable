bf5b8bf5687896d80cfc956055cfea5aae4565de
Syu <konjikicity@gmail.com>
2022-03-21 07:43:26 +0900

    fix: ログイン時の遷移先のpathを修正, not_authenticatedを定義 #29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
+
+  def not_authenticated
+    redirect_to root_path, alert: 'ログインしてください'
+  end
 end
--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,7 +8,7 @@ class OauthsController < ApplicationController
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
-      redirect_back_or_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+      redirect_back_or_to events_path, success: "#{provider.titleize}アカウントでログインしました。"
     else
       begin
         @user = create_from(provider)
