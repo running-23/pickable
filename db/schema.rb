b3f793576556eb02c5862fd476dc607c48c65592
Syu <konjikicity@gmail.com>
2022-03-20 21:40:37 +0900

    feat: github認証に必要なremote_avatar_urlカラムをUsersテーブルに追加 #29

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_130117) do
+ActiveRecord::Schema.define(version: 2022_03_20_123720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_130117) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remote_avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["mattermost_id"], name: "index_users_on_mattermost_id", unique: true
   end
