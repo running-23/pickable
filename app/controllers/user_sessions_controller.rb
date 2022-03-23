162e40d1822f0f85262ee1e37a35506b62542ec6
Syu <konjikicity@gmail.com>
2022-03-23 10:40:43 +0900

    refactor: loginページを削除 #36

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,8 +1,6 @@
 class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new]
 
-  def new; end
-
   def destroy
     logout
     redirect_to root_path, success: 'ログアウトしました'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
-  get 'login', to: 'user_sessions#new'
   delete 'logout', to: 'user_sessions#destroy'
 end
