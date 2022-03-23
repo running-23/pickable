15f99eab767cd75f8601d77c1d85dc1870325ceb
Syu <konjikicity@gmail.com>
2022-03-23 17:06:52 +0900

    refactor: rubocopを実行, 指摘のあった点を修正 #36

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,4 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new]
-
   def destroy
     logout
     redirect_to root_path, success: 'ログアウトしました'
