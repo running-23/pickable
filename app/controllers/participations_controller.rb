aa7238664be0631f0cc21ce27791505ee4c756e6
Mokosu-bit <mokosu755@gmail.com>
2022-03-26 22:31:32 +0900

    refactor: モデルメソッド名変更による変更 #32

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,13 +1,13 @@
 class ParticipationsController < ApplicationController
   def create
     event = Event.find(params[:event_id])
-    current_user.participation(event)
+    current_user.participate(event)
     redirect_to event, success: '参加表明しました'
   end
 
   def destroy
     event = Event.find(params[:event_id])
-    current_user.cansel(event)
+    current_user.cancel(event)
     redirect_to event, success: '参加をキャンセルしました'
   end
 end
