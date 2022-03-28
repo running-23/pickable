a718f7f145b0eef689a2b607d9f63fb60966c1fe
Syu <konjikicity@gmail.com>
2022-03-28 11:48:35 +0900

    feat: user_controllerを修正, ルーティングを追加 #39

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,24 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+before_action :set_user, only: %i[edit show update]
+  def edit; end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to login_path
+  def show; end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), success: 'ユーザー情報が更新されました'
     else
-      render :new
+      render :edit
     end
   end
-
+  
   private
+  
+  def set_user
+    @user = User.find(current_user.id)
+  end
 
   def user_params
-    params.require(:user).permit(:mattermost_id, :password, :password_confirmation, :email,
-                                 :accept_random, :name)
+    params.require(:user).permit(:name, :accept_random, :category_id)
   end
 end
