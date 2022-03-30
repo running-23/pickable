96ae8ddc74144899918ea04e516814a14b7f9242
Syu <konjikicity@gmail.com>
2022-03-30 12:34:45 +0900

    style: ユーザー新規登録後のフラッシュメッセージを変更 #39

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -15,7 +15,7 @@ class OauthsController < ApplicationController
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to edit_user_path(@user), success: 'ユーザー初期設定をしてください'
+        redirect_to edit_user_path(@user), success: '登録ありがとう！ついでに初期設定もよろしく！'
       rescue StandardError
         redirect_to root_path, danger: "#{provider.titleize}アカウントでのログインに失敗しました。"
       end
