8e46d6836663d01e080054fbeea5f059ef832048
jibiking <daiki.jibiki@gmail.com>
2022-04-01 11:44:52 +0900

    feat: イベント一覧の並び順を日時順に変更 #46

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   skip_before_action :require_login, only: %i[index show]
 
   def index
-    @events = Event.includes(%i[user category]).from_today.page(params[:page]).per(10)
+    @events = Event.includes(%i[user category]).from_today.order(:scheduled_date).page(params[:page]).per(10)
   end
 
   def show
