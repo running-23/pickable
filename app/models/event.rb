83397bcfc05c6a42233d5d52f872bf3c11284122
jibiking <daiki.jibiki@gmail.com>
2022-04-01 14:52:45 +0900

    feat: scheduled_dateが過去の場合はじくvalidateを設定 #46

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,6 +33,11 @@ class Event < ApplicationRecord
   validates :number_of_members, presence: true
   validates :scheduled_date, presence: true
   validates :place, presence: true
+  validate :past_scheduled_date
+
+  def past_scheduled_date
+    errors.add(:scheduled_date, 'は、現在時刻以降を入力して下さい') if scheduled_date < Time.current
+  end
 
   def created_by?(user)
     return false unless user
