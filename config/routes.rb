5179e53ee9974e5f7306e7b385e7a936e2e40a18
Syu <konjikicity@gmail.com>
2022-04-11 13:32:43 +0900

    refactor: letter_openerのルーティングをコメントアウトに #52

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'static_pages#top'
   resources :users, only: %i[edit show update]
   resources :categories
@@ -13,4 +12,5 @@ Rails.application.routes.draw do
   get 'oauth/callback', to: 'oauths#callback'
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
   delete 'logout', to: 'user_sessions#destroy'
+  # mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
