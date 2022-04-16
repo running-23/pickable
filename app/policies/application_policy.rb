df3f50c51a294fec68c7367d74b598c125d4ffd8
Mokosu-bit <mokosu755@gmail.com>
2022-04-17 01:39:31 +0900

    fix: scopeは不要のため削除 #70

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -35,19 +35,4 @@ class ApplicationPolicy
   def destroy?
     false
   end
-
-  class Scope
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      raise NotImplementedError, "You must define #resolve in #{self.class}"
-    end
-
-    private
-
-    attr_reader :user, :scope
-  end
 end
