7034ed22eaa4341d91635f56b9d6918ceb799090
jibiking <daiki.jibiki@gmail.com>
2022-04-27 15:55:22 +0900

    style: timescale周りのスタイル修正完了 #127

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   config.action_mailer.delivery_method = :letter_opener_web
   # Uncomment if you wish to allow Action Cable access from any origin.
